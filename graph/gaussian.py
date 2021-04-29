ff54bed0d2c6d24974e963cece2360e48af708bb
Kentaro Seki <kentaro.kpc@gmail.com>
2021-04-29 22:21:13 +0900

    正規分布の確率密度関数を追加

---
+++ graph/gaussian.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import math
+import japanize_matplotlib
+import numpy as np
+import subprocess as sp
+
+def p(x, mu, sigma2):
+    return math.exp(-(x-mu)**2/(2*sigma2))/math.sqrt(2*math.pi*sigma2)
+
+N=100
+ratio = np.array(range(N+1))/N
+
+x = 5*(ratio-0.5)
+y = [p(x_, 0., 1.) for x_ in x]
+
+plt.plot(x,y)
+plt.xlabel("x")
+plt.ylabel("p(x)")
+imgname = 'picture/gaussian.png'
+plt.savefig(imgname)
+
+sp.run(f'mpv {imgname}', shell=True)
+
+
